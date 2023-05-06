9d2658bf0ad63673f4b911717aeafdf533be3f9d
thientrile <thientrile2003@gmail.com>
2023-05-06 11:10:24 +0700

    update cart controller

--- controller/cart.controller.php
+++ controller/cart.controller.php
@@ -1,8 +1,9 @@
 <?php
 
 $method = $_SERVER['REQUEST_METHOD'];
-if ($method == 'POST' && isset($_GET['id']) && isset($_SESSION["s_user"])) {
-    $Cart->addCart($_SESSION["s_user"], $_GET['id']);
+if ($method == 'POST'  && isset($_SESSION["s_user"])) {
+    $Cart->addCart($_SESSION["s_user"], $_POST['id']);
+    echo json_encode(array("status" =>"success"));
 } elseif ($method == 'GET' && isset($_SESSION["s_user"])) {
     $array = array();
     $result = $Cart->viewCart($_SESSION["s_user"]);
@@ -13,4 +14,7 @@ if ($method == 'POST' && isset($_GET['id']) && isset($_SESSION["s_user"])) {
 } elseif ($method == 'PUT') {
 } elseif ($method == 'DELETE') {
     // Method is DELETE
+}
+else{
+    echo json_encode(array("status" =>"fall","method"=>$method,"session_id"=>$_SESSION["s_user"]));
 }
