00e3c6e954e6c0651aaa9afd810805742b6de8d4
thientrile <thientrile2003@gmail.com>
2023-05-06 11:04:58 +0700

    delele function automatically delete session

--- model/main.models.php
+++ model/main.models.php
@@ -1,15 +1,12 @@
 <?php
 /* It's starting a session. */
-session_set_cookie_params(0);
 
-// Start the session
+
+
 session_start();
 
-// Unset all session variables
-$_SESSION = array();
 
-// Destroy the session
-session_destroy();
+
 set_include_path(get_include_path() . PATH_SEPARATOR . './');
 spl_autoload_extensions('.models.php');
 spl_autoload_register();
