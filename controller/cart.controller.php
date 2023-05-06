733038f753539cad8fabea54d3dc914a27ee72a8
thientrile <thientrile2003@gmail.com>
2023-05-06 14:19:11 +0700

    build 'Add to cart' function

--- controller/cart.controller.php
+++ controller/cart.controller.php
@@ -1,14 +1,23 @@
 <?php
 
 $method = $_SERVER['REQUEST_METHOD'];
-if ($method == 'POST'  && isset($_SESSION["s_user"])) {
+if ($method == 'POST'  && isset($_SESSION["s_user"]) &&! $Cart->checkCart($_SESSION["s_user"], $_POST['id'])) {
+
     $Cart->addCart($_SESSION["s_user"], $_POST['id']);
     echo json_encode(array("status" => "success"));
 } elseif ($method == 'GET' && isset($_SESSION["s_user"])) {
-    $array = array();
-    $result = $Cart->viewCart($_SESSION["s_user"]);
-    while ($row = $result->fetch()) {
-        array_push($array, $row);
+
+
+    if (isset($_GET['id'])) {
+
+        $result = $Cart->checkCart($_SESSION["s_user"], $_GET['id']);
+        $array = array("result" => $result);
+    } else {
+        $array = array();
+        $result = $Cart->viewCart($_SESSION["s_user"]);
+        while ($row = $result->fetch()) {
+            array_push($array, $row);
+        }
     }
     echo json_encode($array);
 } elseif ($method == 'PUT') {
